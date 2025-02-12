579cb2cd1bce7e70d5094ee7d8516581406ea757
user <s24022@gsm.ghs.kr>
2025-02-12 14:32:16 +0900

    feat : 2차 퍼블리싱

--- lib/view/sign_up.dart
+++ lib/view/sign_up.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:gsm_certification_system/util/custom_text_field.dart';
 
+import '../util/color.dart';
 import '../util/text.dart';
 
 class SignUpScreen extends StatelessWidget {
-  const SignUpScreen({super.key});
+  SignUpScreen({super.key});
+
+  TextEditingController checkEmailController = TextEditingController();
+  ColorData colorData = ColorData();
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +22,34 @@ class SignUpScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             titleText(text: 'SIGN UP', size: size.width * 0.1),
+            CustomTextField(
+              hintText: '학교 이메일을 입력해주세요',
+              obscureText: false,
+              controller: checkEmailController,
+            ),
+            SizedBox(
+              height: size.height * 0.03,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                  side: BorderSide(
+                    color: Color(colorData.mainColor),
+                    width: 1,
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  maximumSize: Size(size.width, 60),
+                  minimumSize: Size(size.width, 40),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+              child: Text(
+                '인증번호 받기',
+                style: TextStyle(
+                    color: Color(colorData.mainColor),
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+
           ],
         ),
       ),
